673be4b57d40ad5afb24c0df882d460fa64ce322
khalilullahalfaath <75381803+khalilullahalfaath@users.noreply.github.com>
2022-04-02 21:02:48 +0800

    Delete main.py

--- main.py
+++
@@ -1,5 +0,0 @@
-#   Lakukan analisis dan desain algoritma Geneti Algorithm (GA) serta 
-#   mengimplementasikannya ke dalam suatu program komputer untuk mencari nilai 𝑥 dan 𝑦
-#   sehingga diperoleh nilai minimum dari fungsi 
-
-print("Hello world")
